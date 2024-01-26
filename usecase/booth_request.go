347fb41cad4fd2741181473ff9ab20e8eb91db6a
SmallChX <minhthuan55588@gmail.com>
2024-01-26 15:28:41 +0700

    Update booth_request.go

--- usecase/booth_request.go
+++ usecase/booth_request.go
@@ -1,8 +1,10 @@
 package usecase
 
 import (
+	"errors"
 	"jobfair2024/model"
 	"jobfair2024/repository"
+	"time"
 )
 
 // Đây là Request của Company đặt Booth.
@@ -35,6 +37,16 @@ func NewBoothRequestUsecase(
 	}
 }
 
+type BoothRequestInfo struct {
+	BoothIDList []int64   `json:"booth_id"`
+	CompanyID   int64     `json:"company_id"`
+	Type        string    `json:"type"`
+	CreateAt    time.Time `json:"create_at"`
+	// Some for another request
+	Reason                 string  `json:"reason"` // for delete request
+	DestinationBoothIDList []int64 `json:"des_booth_id"`
+}
+
 func (b *boothRequestUsecaseImpl) GetRequest(requestID int64) (*model.BoothRequest, error) {
 	request, err := b.boothRequestRepository.FindByID(requestID)
 	if err != nil {
@@ -51,6 +63,73 @@ func (b *boothRequestUsecaseImpl) GetAllRequest() ([]model.BoothRequest, error) 
 	return requests, nil
 }
 
-func (b *boothRequestUsecaseImpl) CreateRequest(request model.BoothRequest) error {
+func (b *boothRequestUsecaseImpl) CreateRequest(request *BoothRequestInfo) error {
+	// Check role: company
+	switch request.Type {
+	case string(model.RegistTypeRequest):
+		if !isAvailableBooth(b.boothRepository, request.BoothIDList) {
+			return errors.New("booths not available")
+		}
+		if !isContiniousBooths(request.BoothIDList) {
+			return errors.New("booth not continious")
+		}
+	case string(model.ChangeTypeRequest):
+		if len(request.BoothIDList) != len(request.DestinationBoothIDList) {
+			return errors.New("not match booths number")
+		}
+		if !isAvailableBooth(b.boothRepository, request.DestinationBoothIDList) {
+			return errors.New("booths not available")
+		}
+		if !isContiniousBooths(request.DestinationBoothIDList) {
+			return errors.New("booth not continious")
+		}
+		if !isCompanyBoothOwner(b.boothRepository, request.CompanyID, request.BoothIDList) {
+			return errors.New("not match company own booths")
+		}
+	case string(model.RemoveTypeRequest):
+		if !isCompanyBoothOwner(b.boothRepository, request.CompanyID, request.BoothIDList) {
+			return errors.New("not match company own booths")
+		}
 
+	default:
+		return errors.New("no match request type found!")
+	}
+
+	return nil
+}
+
+func isAvailableBooth(boothRepository repository.BoothRepository, boothIDList []int64) bool {
+	for _, boothID := range boothIDList {
+		booth, err := boothRepository.FindByID(boothID)
+		if err != nil {
+			return false
+		}
+
+		if booth.CompanyID != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isContiniousBooths(boothIDList []int64) bool {
+	temp := 0
+	for _, boothID := range boothIDList {
+		if temp != 0 && int(boothID)-temp > 1 {
+			return false
+		}
+		temp = int(boothID)
+	}
+	return true
+}
+
+func isCompanyBoothOwner(boothRepository repository.BoothRepository, companyID int64, boothIDList []int64) bool {
+	for _, boothID := range boothIDList {
+		booth, _ := boothRepository.FindByID(boothID)
+		if booth.CompanyID != companyID {
+			return false
+		}
+	}
+	return true
 }
